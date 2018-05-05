50a1e682a9247de4bf069a005240e4cf900de96b
Merjalei <meleisti@gmail.com>
2018-05-05 11:43:31 +0300

    bite-funktiossa printtinä vielä talo-storytextin alku

--- tickslife.py
+++ tickslife.py
@@ -1,15 +1,18 @@
 import mysql.connector
 import mysql.connector.errors as err
+from enum import Enum
 
+class direction(Enum):
+	NORTH = 0
+	SOUTH = 1
+	WEST = 2
+	EAST = 3
 
-
-db = mysql.connector.connect(
-    host="localhost",
-    user="dbuser",
-    password="dbpass",
-    db="tickslife"
-)
-
+db = mysql.connector.connect(host="localhost",
+                      user="dbuser",
+                      password="dbpass",
+                      db="tickslife")
+storycount=1
 
 def drop():
     cur = db.cursor()
@@ -52,62 +55,76 @@ def smell():
         print("There is no smell of prey here.")
     return 
 
-def distanceToAnimalA(X,Y,lvl): # return integer value of how many steps it takes to find nearest animal
-	return
+def distanceToAnimal(X,Y,lvl):
+	#antti
+	distance = sys.maxint
+
+	return distance
 
 def directionToAnimal(X,Y,lvl):
 	#antti
 	return 
 
 def printNextStory():
-	#joonatan
-	return
+    cur = db.cursor()
+    sql = "SELECT storyID, description FROM story;"
+    cur.execute
+    for row in cur.fetchall():
+        storyID = row[0]
+        story = row[1]
+        if storyID == storycount:
+            print(story)
+            storycount += 1
+            
+return
 
 def bite():
-	#joonatan
-	return
+    cur = db.cursor()
+    sql = "SELECT locationInAnimal.skinThickness, animal.animalID FROM locationInAnimal INNER JOIN tick ON locationInAnimal.locationID=tick.locationID;"
+    cur.execute(sql)
+    for row in cur.fetchall():
+    if row[0]==1:
+        print("Yess! Excellent spot for biting. You become filled with blood just now.")
+        if row[1]==2:
+            sql = "UPDATE tick SET disease = 'lyme disease';"
+            cur.execute(sql)
+            print("The vole you just bit had lyme disease. You now carry this useful 'gun' with you")
+        if row[0]==3:
+            sql = "UPDATE tick SET X=1, Y=3, level = 3,;"
+            cur.execute(sql)
+            print("You bit a running dog. Unfortunately you blood meal was too little. But the dog ran into a man's house.")
+        sql = "SELECT level FROM tick;"
+        cur.execute(sql)
+        for row in cur.fetchall():
+        if row[0]==1:
+            sql = "UPDATE tick SET X=3, Y=2, level=2;"
+        elif row[0]==2:
+            sql = "UPDATE tick SET X=1, Y=6, level=4;"
+        elif row[0] ==3:
+            sql = "UPDATE tick SET X=1, Y06, level=4;"
+        cur.execute(sql)    
+        printNextStory() 
+    else:
+        print("The skin was too thick, you couldn't bite here. You fell off.")
+        sql="UPDATE tick SET locationID = NULL, animalID = NULL;" 
+        cur.execute(sql)
+
+    return
 
 def moveInAnimal(direction):
 	#antti
 	return
 
-#
-def possibleMovementsInAnimal():
-	possibleMovements = []
-	cur = db.cursor()
-	sql = "SELECT locationInAnimal.name FROM tick INNER JOIN route \
-	ON tick.locationID = route.locationID AND tick.animalID = route.animalID\
-	INNER JOIN locationInAnimal ON locationInAnimal.locationID = route.locationToID;"
-	cur.execute(sql)
-	for row in cur.fetchall():
-		possibleMovements.append(row[0])
-	return possibleMovements
-
 def animalMove():
-	#antti
+	#antti	
 	return
 
-def isTickVulnerable():
-	cur = db.cursor()
-	sql = "SELECT description.color FROM tick INNER JOIN tile\
-	ON tick.X = tile.X AND tick.Y = tile.Y AND tick.level = tile.level\
-	INNER JOIN description\
-	ON description.X = tick.X AND description.Y = tick.Y AND description.level = tick.level;"
-	cur.execute(sql)
-	for row in cur.fetchall():
-
+def IsTickInAnimal():
+	#antti
 	if asd:
 		return true
 	else:
 		return false
-
-def IsTickInAnimal():
-
-	#antti
-	#if asd:
-		#return true
-	#else:
-		#return false
 
 def endOfTurn():
 	BLUE1 = "\033[94m"
@@ -119,10 +136,10 @@ def endOfTurn():
 	sql = "SELECT description, color FROM description INNER JOIN tick ON description.X = tick.X AND description.Y = tick.Y AND description.level = tick.level;"
 	cur.execute(sql)
 	for row in cur.fetchall():
-                if row[1] == 2:
-                        print(GREEN2)
+		if row[1] == 2:
+			print(GREEN2)
 		elif row[1] == 3:
-                        print(BROWN3)
+			print(BROWN3)
 		print(row[0])
 	print(ENDC4)
 	return 
@@ -137,6 +154,7 @@ def tickMove(direction):
 		y = row[1]
 		level = row[2]
 		time = row[3]
+	#if x != asd and y != asd and lvl != asd: #joonatan
 	if direction == "north":
 		sql = "UPDATE tick SET Y = "+str(y-1)+";"
 	if direction == "south":
@@ -147,41 +165,15 @@ def tickMove(direction):
 		sql = "UPDATE tick SET X = "+str(x+1)+";"
 	if direction == "still":
 		sql = "UPDATE tick SET timeVisible = "+str(time+1)+";"
-	if direction == "down" and y == 100:
-		if x == 100 and level == 2:
-			sql = "UPDATE tick SET X = 1, Y = 2;"
-		if x == 200 and level == 2:
-			sql = "UPDATE tick SET X = 3, Y = 4;"
-		if x == 300 and level == 2:
-			sql = "UPDATE tick SET X = 4, Y = 2;"
-		if x == 100 and level == 3:
-			sql = "UPDATE tick SET X = 1, Y = 1;"
-		if x == 200 and level == 3:
-			sql = "UPDATE tick SET X = 2, Y = 2;"
-		if x == 300 and level == 3:
-			sql = "UPDATE tick SET X = 3, Y = 3;"
-		if x == 100 and level == 4:
-			sql = "UPDATE tick SET X = 5, Y = 4;"
-	if direction == "climb":
-		if x == 1 and y == 2 and level == 2:
-			sql = "UPDATE tick SET X = 100, Y = 100;"
-		if x == 3 and y == 4 and level == 2:
-			sql = "UPDATE tick SET X = 200, Y = 100;"
-		if x == 4 and y == 2 and level == 2:
-			sql = "UPDATE tick SET X = 300, Y = 100;"
-		if x == 1 and y == 1 and level == 3:
-			sql = "UPDATE tick SET X = 100, Y = 100;"
-		if x == 2 and y == 2 and level == 3:
-			sql = "UPDATE tick SET X = 200, Y = 100;"
-		if x == 3 and y == 3 and level == 3:
-			sql = "UPDATE tick SET X = 300, Y = 100;"
-		if x == 2 and y == 5 and level == 4:
-			sql = "UPDATE tick SET X = 100, Y = 100;"
 	try:
 		cur.execute(sql)
 	except err.IntegrityError:
 		print("You can't go there")
 	return
+	#else: #joonatan
+	#	if direction == "climb":
+	#		sql = "UPDATE tick SET X = ;"
+	#	return
 
 cmd = ""
 
@@ -209,8 +201,6 @@ while cmd != 'exit':
 			tickMove("west")
 		elif command[1] == "east" or command[1] == "e" or command[1] == "E":
 			tickMove("east")
-		elif command[1] == "down":
-			tickMove("down")
 		else:#error message when go commands parameter is wrong
 			print("")
 			print("-- ", end = "")
@@ -224,8 +214,6 @@ while cmd != 'exit':
 	if command[0] == "help":
 		print("possible commands:")
 		print("go")
-	if command [0] == "climb":
-		tickMove("climb")
 
 	print(" --- ")
 
